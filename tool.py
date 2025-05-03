13adaf46d216ecfa681649a7170c1a6956877fac
RJTPP <rjt.tham@gmail.com>
2025-05-04 02:59:38 +0700

    perf: Optimize import

--- tool.py
+++ tool.py
@@ -17,7 +17,6 @@ import re
 from pydantic import BaseModel, Field
 from datetime import datetime
 from collections import deque
-from typing import Any, List, Optional, Union
 
 def pwd():
     return os.getcwd()
